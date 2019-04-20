e76fd86f61c1636c7ee3496fb53feda3705498ed
DRRoca <demsrey.roca@gmail.com>
2019-04-20 20:29:31 +0800

    login page added

--- app/src/main/java/com/usep/isdako/MainActivity.kt
+++ app/src/main/java/com/usep/isdako/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.usep.isdako
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentTransaction
+//import android.support.v4.app.FragmentManager
 import android.widget.Toast
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
@@ -14,17 +17,25 @@ import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoginFragment.OnFragmentInteractionListener {
 //    private var mapView: MapView? = null
 //    private var permissionsManager: PermissionsManager? = null
 //    private var mapboxMap: MapboxMap? = null
-
+    lateinit var loginFragment: LoginFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val intentLocationComponent: Intent = Intent (this, LocationComponentActivity::class.java)
+        loginFragment = LoginFragment.newInstance()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, loginFragment)
+            .addToBackStack(loginFragment.toString())
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+//        val intentLocationComponent: Intent = Intent (this, LocationComponentActivity::class.java)
 
-        startActivity(intentLocationComponent)
+//        startActivity(intentLocationComponent)
     }
 
 
@@ -45,4 +56,7 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
     }
 
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
