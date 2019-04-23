a3dc30e61aba3b0e48ac9ef634a7af77685637ef
DRRoca <demsrey.roca@gmail.com>
2019-04-23 13:21:26 +0800

    Firebase auth reconfigured

--- app/src/main/java/com/usep/isdako/MainActivity.kt
+++ app/src/main/java/com/usep/isdako/MainActivity.kt
@@ -6,36 +6,30 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.FragmentTransaction
 //import android.support.v4.app.FragmentManager
-import android.widget.Toast
-import com.mapbox.android.core.permissions.PermissionsListener
-import com.mapbox.android.core.permissions.PermissionsManager
-import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.location.modes.CameraMode
-import com.mapbox.mapboxsdk.location.modes.RenderMode
-import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
-import com.mapbox.mapboxsdk.maps.Style
 
 class MainActivity : AppCompatActivity(), LoginFragment.OnFragmentInteractionListener {
-//    private var mapView: MapView? = null
-//    private var permissionsManager: PermissionsManager? = null
-//    private var mapboxMap: MapboxMap? = null
-    lateinit var loginFragment: LoginFragment
+
+//    lateinit var loginFragment: LoginFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        loginFragment = LoginFragment.newInstance()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.container, loginFragment)
-            .addToBackStack(loginFragment.toString())
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            .commit()
-//        val intentLocationComponent: Intent = Intent (this, LocationComponentActivity::class.java)
+//          **Fragment Template
 
-//        startActivity(intentLocationComponent)
+//        loginFragment = LoginFragment.newInstance()
+//        supportFragmentManager
+//            .beginTransaction()
+//            .replace(R.id.container, loginFragment)
+//            .addToBackStack(loginFragment.toString())
+//            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+//            .commit()
+//
+//          ** Activity Template
+        val activityIntent = Intent (this, StartActivity::class.java)
+
+        startActivity(activityIntent)
+
+
     }
 
 
